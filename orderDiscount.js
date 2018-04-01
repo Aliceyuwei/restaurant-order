213e4487f7b169560bd95d74f497cc50af020254
AliceLin <ke590pjmj7892@gmail.com>
2018-04-01 23:21:10 +0800

    update

--- orderDiscount.js
+++ orderDiscount.js
@@ -11,3 +11,31 @@ nowPeople = 1;
 var ans = sumPeople !== nowPeople;
 console.log(ans);
 document.getElementById('ans2').textContent = ans + '，是得沒錯!';
+
+// var farms = [
+//     {
+//         farmer:'卡斯柏',
+//         field:6,
+//         chick:200,
+//         banana:5000
+//     },
+//     {
+//         farmer:'查理',
+//         field:10,
+//         chick:50,
+//         banana:5000
+//     },
+//     {
+//         farmer:'約翰',
+//         field:6,
+//         chick:120,
+//         banana:5000
+//     }
+// ]
+// var farmsTotal = farms.length;
+// console.log(farmsTotal);
+// var bananaTotal = 0;
+// for(var i = 0; i<farmsTotal; i++){
+//     bananaTotal = bananaTotal+farms[i].banana;
+// }
+// console.log(bananaTotal);
